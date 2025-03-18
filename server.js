a50f4d3b2384f137c68fe3ed32d927baad31447b
Muhammad Shayan <shayanarif666@gmail.com>
2025-03-18 13:33:26 +0500

    Backend

--- server.js
+++ server.js
@@ -5,7 +5,23 @@ const { dbConnection } = require("./config/config");
 
 // Built in Middlewares
 const app = express();
-app.use(cors());
+
+// Allowed Origins
+const allowedOrigins = ["http://localhost:5173", "https://lms-backend-three-dusky.vercel.app/"];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: "GET, POST, PUT, DELETE, OPTIONS",
+    allowedHeaders: "Content-Type, Authorization",
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
